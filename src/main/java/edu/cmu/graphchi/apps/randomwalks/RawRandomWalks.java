0b184959f5fab77b9ba9257a7046cc75b65a8707
rwang067 <wangrui1994@126.com>
2019-07-11 14:21:45 +0800

    close counting utilization

--- src/main/java/edu/cmu/graphchi/apps/randomwalks/RawRandomWalks.java
+++ src/main/java/edu/cmu/graphchi/apps/randomwalks/RawRandomWalks.java
@@ -38,11 +38,10 @@ public class RawRandomWalks implements WalkUpdateFunction<EmptyType, EmptyType> 
     private int R;
     private int L;
     private int s;
-    // private String companionUrl;
-    // 20190619 by Rui -- used for counting IO utilizations
-    int nThreads;
-    private int[] numedges;
-    private int[] used_edges;
+    // // 20190619 by Rui -- used for counting IO utilizations
+    // int nThreads;
+    // private int[] numedges;
+    // private int[] used_edges;
 
     public RawRandomWalks(String companionUrl, String baseFilename, int nShards, int N, int R, int L, int s) throws Exception{
         this.baseFilename = baseFilename;
@@ -55,22 +54,22 @@ public class RawRandomWalks implements WalkUpdateFunction<EmptyType, EmptyType> 
         this.L = L;
         this.s = s;
 
-        // /////////////////////////
-        (new File("drunkardmob_utilization.csv")).delete();
+        // // /////////////////////////
+        // (new File("drunkardmob_utilization.csv")).delete();
 
-        numedges = new int[nShards];
-        BufferedReader rd = new BufferedReader(new FileReader(new File(ChiFilenames.getFilenameIntervals(baseFilename, nShards)+".edgenums")));
-        String line;
-        for(int i = 0; i < nShards; i++) {
-            line = rd.readLine();
-            numedges[i] = Integer.parseInt(line);
-        }
+        // numedges = new int[nShards];
+        // BufferedReader rd = new BufferedReader(new FileReader(new File(ChiFilenames.getFilenameIntervals(baseFilename, nShards)+".edgenums")));
+        // String line;
+        // for(int i = 0; i < nShards; i++) {
+        //     line = rd.readLine();
+        //     numedges[i] = Integer.parseInt(line);
+        // }
         
-        nThreads = Runtime.getRuntime().availableProcessors();
-        used_edges = new int[nThreads];
-        for(int i=0; i<nThreads; i++){
-            used_edges[i] = 0;
-        }
+        // nThreads = Runtime.getRuntime().availableProcessors();
+        // used_edges = new int[nThreads];
+        // for(int i=0; i<nThreads; i++){
+        //     used_edges[i] = 0;
+        // }
     }
 
     private void execute(int numIters) throws Exception {
@@ -116,7 +115,7 @@ public class RawRandomWalks implements WalkUpdateFunction<EmptyType, EmptyType> 
             if (numOutEdges > 0) {
                 
                 //***********Rui************
-                used_edges[(int)(Thread.currentThread().getId())%nThreads] += numWalks;
+                // used_edges[(int)(Thread.currentThread().getId())%nThreads] += numWalks;
 
                 for(int i=0; i < numWalks; i++) {
                     int walk = walks[i];
@@ -171,23 +170,23 @@ public class RawRandomWalks implements WalkUpdateFunction<EmptyType, EmptyType> 
 
     @Override
     public void compUtilization(int execInterval){
-        logger.info("compUtilization...");
-        for(int i = 1; i < nThreads; i++){
-            used_edges[0] += used_edges[i];
-        }
-        float utilization = (float)used_edges[0] / (float)numedges[execInterval];
-        try{
-            FileWriter writer = new FileWriter("drunkardmob_utilization.csv", true);   
-            writer.write(execInterval + "\t" + numedges[execInterval] + "\t" + used_edges[0] + "\t" + utilization + "\n" );   
-            writer.close();
-        } catch(IOException ie) {
-            ie.printStackTrace();
-        } 
-        // logstream(LOG_DEBUG) << "IO utilization = " << utilization << std::endl;
+        // logger.info("compUtilization...");
+        // for(int i = 1; i < nThreads; i++){
+        //     used_edges[0] += used_edges[i];
+        // }
+        // float utilization = (float)used_edges[0] / (float)numedges[execInterval];
+        // try{
+        //     FileWriter writer = new FileWriter("drunkardmob_utilization.csv", true);   
+        //     writer.write(execInterval + "\t" + numedges[execInterval] + "\t" + used_edges[0] + "\t" + utilization + "\n" );   
+        //     writer.close();
+        // } catch(IOException ie) {
+        //     ie.printStackTrace();
+        // } 
+        // // logstream(LOG_DEBUG) << "IO utilization = " << utilization << std::endl;
 
-        for(int i=0; i<nThreads; i++){
-            used_edges[i] = 0;
-        }
+        // for(int i=0; i<nThreads; i++){
+        //     used_edges[i] = 0;
+        // }
     }
 
     public static void main(String[] args) throws Exception {
