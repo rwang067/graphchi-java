103ade3508cbd00b090ed0920143889d0bcfcd80
rwang067 <wangrui1994@126.com>
2019-08-31 20:20:23 +0800

    graphFilePrefix

--- src/main/java/edu/cmu/graphchi/ChiFilenames.java
+++ src/main/java/edu/cmu/graphchi/ChiFilenames.java
@@ -24,8 +24,8 @@ import java.util.ArrayList;
  */
 public class ChiFilenames {
 
-    // public static String graphFilePrefix = "/../../raid0_defghij/Crawl_DrunkardMob/";
-    public static String graphFilePrefix = "_DrunkardMob/";
+    public static String graphFilePrefix = "/../../raid0_defghij/Crawl_DrunkardMob/";
+    // public static String graphFilePrefix = "_DrunkardMob/";
     // public static String graphFilePrefix = "_DrunkardMob/";
 
     public static String vertexDataSuffix = "";
